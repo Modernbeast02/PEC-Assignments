86bf9738c060aad5e6f82dcb8fe2cfe64d8b90f5
Modernbeast02 <89138051+Modernbeast02@users.noreply.github.com>
2022-01-07 16:19:43 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -6,6 +6,7 @@ num4 = num1 + num2 + num3
 average = num4 / 3
 print("Average is : ", average)
 
+
 # Ques2
 Gross_Income = int(input("Enter the Gross Income : "))
 Dependent = int(input("Enter Dependent : "))
@@ -42,8 +43,48 @@ print("Sorted Order Of Marks : ", Marks)
 
 
 # # Ques 5
-Student_Details = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-Student_Details.remove('Black')
-print("Part a :", Student_Details)
-Student_Details[3] = 'Purple'  # Replacing Pink with Purple
-print("Part b :", Student_Details)
+Color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+List_for_partA = Color
+List_for_partA.remove('Black')
+print("Part a :", List_for_partA)
+Color[3:5] = ['Purple']  # Replacing Black and Pink with Purple
+print("Part b :", Color)
+
+# -----------------------------------------------------------------------------------------
+
+Output:
+# Ques1
+Enter First Number : 5Output:
+Enter First Number : 5
+Enter Second Number : 10
+Enter Third Number : 15
+Average is :  10.0
+
+ 
+# Ques2
+Enter the Gross Income : 1000000
+Enter Dependent : 2
+Your income tax :  196800.0
+    
+# Ques3
+Enter SID: 21103028
+Enter Name: Ankur
+Enter Gender: Male
+Enter Course Name: Introduction to Computing
+Enter CGPA: 9.90
+Student Details : [21103028, 'Ankur', 'Male', 'Introduction to Computing', 9.9]
+
+# Ques4
+Enter Marks 1 : 90 
+Enter Marks 2 : 99
+Enter Marks 3 : 95
+Enter Marks 4 : 98 
+Enter Marks 5 : 93
+Sorted Order Of Marks :  [90, 93, 95, 98, 99]
+    
+# Ques5
+Part a : ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b : ['Red', 'Green', 'White', 'Purple', 'Yellow']
+
+
+    

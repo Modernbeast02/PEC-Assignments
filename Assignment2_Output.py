8c922e6090aee9ba19d1345146fbc98e2a0fe248
Modernbeast02 <89138051+Modernbeast02@users.noreply.github.com>
2022-01-17 16:35:44 +0530

    Create Assignment2_Output.py

---
+++ Assignment2_Output.py
@@ -0,0 +1,39 @@
+Ques 1
+Length of original string is 35
+Reversed string -- egaugnal evitisnes esac a si nohtyP
+Index of 'a' in original string is 10
+Original String after removing whitespace -- Pythonisacasesensitivelanguage
+
+
+Ques 2
+Hey, Ankur Gupta Here!
+My SID is 21103028
+I am from CSE and my CGPA is 10
+
+
+Ques 3
+8
+58
+50
+224
+40
+14
+0
+
+Ques 4
+Enter the number: 10
+Enter the number: 20
+Enter the number: 30
+Max number is 30
+
+
+Ques 5
+Enter the string: sjnfjfaname
+Yes
+
+
+Ques 6
+Enter Triangle Side Length: 5
+Enter Triangle Side Length: 6
+Enter Triangle Side Length: 3
+Yes

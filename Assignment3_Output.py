d48adddb75f246225a928c63c591eeb5ff86ccef
Modernbeast02 <89138051+Modernbeast02@users.noreply.github.com>
2022-02-01 17:10:55 +0530

    Create Assignment3_Output.py

---
+++ Assignment3_Output.py
@@ -0,0 +1,69 @@
+OUTPUTS
+
+Ques 1
+Enter the string: I have made a hashmap to solve this question question 
+{'I': 1, 'have': 1, 'made': 1, 'a': 1, 'hashmap': 1, 'to': 1, 'solve': 1, 'this': 1, 'question': 2}
+
+Ques 2
+Enter the date: 28
+Enter the month: 02
+Enter the year: 1999
+Next Date is: 1/03/1999
+
+Ques 3
+Enter total numbers to be added to list: 3
+3
+9
+10
+[(3, 9), (9, 81), (10, 100)]
+
+Ques 4
+Enter Grade: 9
+Your Grade is A and Excellent Performance
+
+Ques 5
+ABCDEFGHIJK 
+ ABCDEFGHI 
+  ABCDEFG 
+   ABCDE 
+    ABC 
+     A 
+   
+  
+Ques 6  
+
+Do you want to enter? Y
+Enter SID: 21103028
+Enter name: Ankur
+Do you want to enter? Y
+Enter SID: 21110302
+Enter name: ABCD  
+Do you want to enter? Y
+Enter SID: 24353562
+Enter name: XYZ
+Do you want to enter? N
+Part A {21103028: 'Ankur', 21110302: 'ABCD', 24353562: 'XYZ'}
+Part B [(21110302, 'ABCD'), (21103028, 'Ankur'), (24353562, 'XYZ')]
+Part C [(21103028, 'Ankur'), (21110302, 'ABCD'), (24353562, 'XYZ')]
+Enter SID to search: 21103028
+Part d  Ankur
+
+Ques 7
+Enter number for fibonacci: 5
+0
+1
+1
+2
+3
+average is : 1.4
+  
+Ques 8
+
+Part a {1, 3, 5}
+Part b {3, 6, 8, 9, 13, 17}
+Part c {1, 2, 4, 5}
+part d {6, 7, 8, 9, 10}
+part e {10, 7}
+
+
+  

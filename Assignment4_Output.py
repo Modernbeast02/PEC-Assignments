e4a493b7625fd0ffb3e0f79ba6bfe1582481f540
Modernbeast02 <89138051+Modernbeast02@users.noreply.github.com>
2022-02-26 23:27:32 +0530

    Create Assignment4_Output.py

---
+++ Assignment4_Output.py
@@ -0,0 +1,53 @@
+#Ques 1
+
+Move disk 1 from source A to destination B
+Move disk 2 from source A to destination C
+Move disk 1 from source B to destination C
+Move disk 3 from source A to destination B
+Move disk 1 from source C to destination A
+Move disk 2 from source C to destination B
+Move disk 1 from source A to destination B
+
+#Ques 2
+
+      1        
+     1 1       
+    1 2 1      
+   1 3 3 1     
+  1 4 6 4 1    
+  
+      1        
+     1 1       
+    1 2 1      
+   1 3 3 1     
+  1 4 6 4 1    
+  
+#Ques 3
+
+False
+False
+NO
+Filtered out numbers that are greater than 4 are : [8, 9, 5, 9, 6, 10]
+Set: {5, 6, 8, 9, 10}
+Immutable set is : frozenset({5, 6, 8, 9, 10})
+Hash value of the max value from the above set: 10
+  
+#Ques 4
+Ankur
+21103028
+Object destroyed
+
+#Ques 5
+
+The updated salary of Mehak is 70000 
+Record of Viren deleted
+
+#Ques 6
+George's word: alice
+Barbie's word: celia
+Friendship is Nice
+
+George's word: alice
+Barbie's word: ajkdf
+Fake Friendship
+  

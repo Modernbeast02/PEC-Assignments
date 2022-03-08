c10601cbfb666e80142d5f37199a203925aced89
Modernbeast02 <89138051+Modernbeast02@users.noreply.github.com>
2022-03-08 23:46:25 +0530

    Create Assignment5_Output.py

---
+++ Assignment5_Output.py
@@ -0,0 +1,20 @@
+# Ques4
+
+Input:
+23 12 33 34
+Output:
+[12, 23, 33, 34]
+  
+# Ques 5
+
+Input:
+11
+4 5 2 4 5 6 2 3 4 5 7
+4
+Output
+
+Sorted array is: [2, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7]
+Value is at index: 5
+Total number of occurances is: 3
+  
+  
